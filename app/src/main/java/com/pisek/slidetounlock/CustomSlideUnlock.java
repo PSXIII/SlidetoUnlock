40fa2234ef22318b4748880679f35ca4a5c0c81e
Sek <pisekpis@gmail.com>
2017-06-14 14:31:25 +0700

    Update UI

--- app/src/main/java/com/pisek/slidetounlock/CustomSlideUnlock.java
+++ app/src/main/java/com/pisek/slidetounlock/CustomSlideUnlock.java
@@ -2,7 +2,6 @@ package com.pisek.slidetounlock;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -44,24 +43,24 @@ public class CustomSlideUnlock extends FrameLayout {
     }
 
     private void initInstance() {
-        mBinding.customSlideUnlock.setOnTouchListener(new OnTouchListener() {
+        mBinding.seekBar.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         if (indicator.getBounds().contains((int) event.getX(), (int) event.getY()))
-                            mBinding.customSlideUnlock.onTouchEvent(event);
+                            mBinding.seekBar.onTouchEvent(event);
                         else
                             return false;
                         break;
                     case MotionEvent.ACTION_UP:
-                        if (mBinding.customSlideUnlock.getProgress() > percent)
-                        handleSlide();
+                        if (mBinding.seekBar.getProgress() > percent)
+                            handleSlide();
 
-                        mBinding.customSlideUnlock.setProgress(0);
+                        mBinding.seekBar.setProgress(0);
                         break;
                     default:
-                        mBinding.customSlideUnlock.onTouchEvent(event);
+                        mBinding.seekBar.onTouchEvent(event);
                         break;
                 }
                 return true;
@@ -74,12 +73,26 @@ public class CustomSlideUnlock extends FrameLayout {
     }
 
     public void setIndicator(Drawable indicator) {
-        mBinding.customSlideUnlock.setThumb(indicator);
-        this.indicator = indicator;
+        if (indicator == null)
+            this.indicator = getResources().getDrawable(R.mipmap.ic_launcher);
+        else
+            this.indicator = indicator;
+        mBinding.seekBar.setThumb(indicator);
+        mBinding.seekBar.setThumbOffset(38);
     }
 
     public void setPercentAction(Integer percent) {
-        this.percent = percent;
+        if (percent == null)
+            this.percent = 0;
+        else
+            this.percent = percent;
+    }
+
+    public void setTitle(String title) {
+        if (title == null)
+            mBinding.tvTitle.setText("Slide to unlock");
+        else
+            mBinding.tvTitle.setText(title);
     }
 
     public void setSlideButtonListener(SlideButtonListener listener) {
--- app/src/main/java/com/pisek/slidetounlock/MainActivity.java
+++ app/src/main/java/com/pisek/slidetounlock/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        mBinding.customSlideUnlock.setIndicator(getResources().getDrawable(R.drawable.ic_lock_open));
+        mBinding.customSlideUnlock.setIndicator(getResources().getDrawable(R.drawable.ic_slide_unlock));
         mBinding.customSlideUnlock.setPercentAction(95);
+        mBinding.customSlideUnlock.setTitle("Slide to unlock");
         mBinding.customSlideUnlock.setSlideButtonListener(new CustomSlideUnlock.SlideButtonListener() {
             @Override
             public void handleSlide() {
